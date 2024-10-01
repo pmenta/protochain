ad9e4e912496b987f61dc766a16579d11999cecc
João Martins <joao.martinsm@pm.me>
2024-10-01 20:26:55 -0300

    chore: change test runner to vitest

--- src/application/block/block.spec.ts
+++ src/application/block/block.spec.ts
@@ -1,14 +1,20 @@
-import { describe, expect, test } from "bun:test";
+import { describe, expect, test } from "vitest";
 import { Block } from ".";
 
 describe("Block tests", () => {
   test("Should BE valid", () => {
     const block = new Block(1, "Genesis", "Lorem");
-    expect(block.isValid()).toBeTrue();
+    expect(block.isValid()).toBeTruthy();
   });
 
   test("Should NOT be valid (index)", () => {
     const block = new Block(-1, "Genesis", "Lorem");
-    expect(block.isValid()).toBeFalse();
+    expect(block.isValid()).toBeFalsy();
+  });
+
+  test("Should have a valid sha256 hash", () => {
+    const block = new Block(1, "Genesis", "Lorem");
+    const sha256Regex = /^[a-f0-9]{64}$/;
+    expect(sha256Regex.test(block.hash)).toBeTruthy();
   });
 });
